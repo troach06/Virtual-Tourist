12f752a34895b3e5b016959bc1d07994c3b9bdef
Tyler C. Roach <tyler@bricksimple.com>
2018-07-30 13:25:07 -0400

    Fixed file structure

---
+++ Assets/Scripts/ActivateInstructions.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateInstructions : MonoBehaviour {
+    public GameObject instructions, intro, player;
+    public AudioClip instructionsSound, selectSound;
+    AudioSource audioSource;
+	// Use this for initialization
+	void Start () {
+        audioSource = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    // Activate instructions panel
+    public void Activate()
+    {
+        player.GetComponent<AudioSource>().PlayOneShot(selectSound);
+        intro.SetActive(false);
+        instructions.SetActive(true);
+        iTween.ScaleTo(instructions, new Vector3(0.0036f, 0.002f, 0.003f), 1);
+        audioSource.Stop();
+        audioSource.PlayOneShot(instructionsSound);
+    }
+}
---
+++ Assets/Scripts/Teleporter.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporter : MonoBehaviour
+{
+    public GameObject player, user;
+    public AudioClip[] narrationSounds;
+
+    // Use this for initialization
+    void Start()
+    {
+        user = Camera.main.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    // Teleport player to pad location
+    public void Teleport(GameObject pad)
+    {
+        user.GetComponent<AudioSource>().Stop();
+        player.GetComponent<AudioSource>().Play();
+        PlayAudioClip(pad.tag);
+        player.transform.position = new Vector3(pad.transform.position.x, Camera.main.transform.position.y, pad.transform.position.z);
+    }
+
+    // Play narration audio
+    public void PlayAudioClip(string clipToPlay)
+    {
+        foreach (AudioClip clip in narrationSounds)
+        {
+            if (clip.name == clipToPlay)
+                GetComponent<AudioSource>().PlayOneShot(clip);
+        }
+    }
+}
